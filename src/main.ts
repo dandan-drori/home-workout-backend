b1e7a7cf7df41e5b95391862055efc70d10d7f64
dandan <dandan.drori@zoominfo.com>
2022-06-05 01:39:08 +0300

    fix: add external port to listen to

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('html');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
